f3efc00cef0ac61eea38b88d93913a9cf9867a48
nopifacil <116685243+nopifacil@users.noreply.github.com>
2022-10-25 17:09:58 -0500

    seguridad social

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <title>nopifacil</title> 
+  <meta charset="UTF-8">   
+  <link rel="stylesheet" href="estilos.css"> 
+</head>
+
+<body>
+  <center><h1>Seguridad social y nómina</h1></center>
+  <body bgcolor="">
+  
+  <p><a href="http://nopifacil.byethost7.com/">ir a nopi</a></p> <p> <a href="https://unioncolombia.com//">ir a Union Colombia</a></p>
+
+  <div class="banner"></div>
+
+</body>
+
+</html>
